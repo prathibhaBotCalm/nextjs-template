c0bde7e6f9ce1912a628d825b744a470849a459d
prathibhaBotCalm <prathibha.r@botcalm.com>
2025-03-25 17:02:41 +0530

    chore: initialize app

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import {FlatCompat} from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      ".next/**",
+      "*.css",
+      "dist/**",
+      "esm/**",
+      "public/**",
+      "tests/**",
+      "scripts/**",
+      "*.config.js",
+      "node_modules/**",
+      "coverage/**",
+      "build/**",
+    ],
+  },
+];
+
+export default eslintConfig;
